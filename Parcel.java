c3b6b48be1358539ce9b9b54035415595886287f
nanenmalik <nasreen.malik@live.iium.edu.my>
2023-12-20 23:50:03 +0800

    update Parcel class

--- Parcel.java
+++ Parcel.java
@@ -1,28 +1,15 @@
-//Parcel.java
-import java.util.Date;
-
 public class Parcel {
+    private String recipientName;
+    private int houseNumber;
+    private String content;
 
-   private int houseNumber;
-   private String itemName;
-   private Date storageDate;
+    public Parcel(String recipientName, int houseNumber, String content) {
+        this.recipientName = recipientName;
+        this.houseNumber = houseNumber;
+        this.content = content;
+    }
 
-   public Parcel(int houseNumber, String itemName) {
-       this.houseNumber = houseNumber;
-       this.itemName = itemName;
-       this.storageDate = new Date();
-   }
-
-   public int getHouseNumber() {
-       return houseNumber;
-   }
-
-   public String getItemName() {
-       return itemName;
-   }
-
-   public Date getStorageDate() {
-       return storageDate;
-   } 
-   
+    public int getHouseNumber() {
+        return houseNumber;
+    }
 }
