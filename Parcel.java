fb45ae6e2babe2677af0d579ad195a6960758277
nabilaradiah <nabila.radiah@live.iium.edu.my>
2023-12-21 23:45:30 +0800

    class for the management of the parcel

--- Parcel.java
+++ Parcel.java
@@ -1,11 +1,56 @@
-//Parcel.java
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Parcel {
+    private String recipientName;
+    private int houseNumber;
+    private String content;
+    private LocalDate storedDate;
+    private double parcelCost;
 
-   private int parcelID;
-   private int senderID;
-   private int receiverID;
-   private int status;
-   private String parcelType;
+    public Parcel(String recipientName, int houseNumber, String content) {
+        this.recipientName = recipientName;
+        this.houseNumber = houseNumber;
+        this.content = content;
+        this.storedDate = LocalDate.now();
+        this.parcelCost = 0;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isExpired() {
+        LocalDate currentDate = LocalDate.now();
+        long daysDifference = ChronoUnit.DAYS.between(storedDate, currentDate);
+        return daysDifference > 2;
+    }
+    
+    public void handleExtendTime(int days) {
+        storedDate = storedDate.plusDays(days);
+        parcelCost += days * 3;
+    }
+    
+    public void handleReturn() {
+        storedDate = LocalDate.now();
+    }
+       
+    public double getParcelCost() {
+        return parcelCost;
+    }
+       
+
+    @Override
+    public String toString() {
+        return "Parcel for " + recipientName + " at " + houseNumber + " containing " + content;
+    }
    
 }
