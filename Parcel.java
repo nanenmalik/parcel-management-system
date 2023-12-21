cc798a108555782df5cd6d0df3e38ffc021e2051
nanenmalik <nasreen.malik@live.iium.edu.my>
2023-12-21 12:13:19 +0800

    modify parcel class

--- Parcel.java
+++ Parcel.java
@@ -1,12 +1,19 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Parcel {
     private String recipientName;
     private int houseNumber;
     private String content;
+    private LocalDate storedDate;
+    private double parcelCost;
 
     public Parcel(String recipientName, int houseNumber, String content) {
         this.recipientName = recipientName;
         this.houseNumber = houseNumber;
         this.content = content;
+        this.storedDate = LocalDate.now();
+        this.parcelCost = 0;
     }
 
     public int getHouseNumber() {
@@ -21,5 +28,29 @@ public class Parcel {
         return content;
     }
 
+    public boolean isExpired() {
+        LocalDate currentDate = LocalDate.now();
+        long daysDifference = ChronoUnit.DAYS.between(storedDate, currentDate);
+        return daysDifference > 2;
+    }
     
+    public void handleExtendTime(int days) {
+        storedDate = storedDate.plusDays(days);
+        parcelCost += days * 3;
+    }
+    
+    public void handleReturn() {
+        storedDate = LocalDate.now();
+    }
+       
+    public double getParcelCost() {
+        return parcelCost;
+    }
+       
+
+    @Override
+    public String toString() {
+        return "Parcel for " + recipientName + " at " + houseNumber + " containing " + content;
+    }
+   
 }
