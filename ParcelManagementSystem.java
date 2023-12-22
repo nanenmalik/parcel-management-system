dfbdabc21b466b8c25c005bf181c6867eb177ad4
nabilaradiah <nabila.radiah@live.iium.edu.my>
2023-12-22 23:40:14 +0800

    Add file I/O functionality and slot availability check

--- ParcelManagementSystem.java
+++ ParcelManagementSystem.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ParcelManagementSystem {
 
@@ -12,12 +17,23 @@ public class ParcelManagementSystem {
         }
     }
 
-    public void storeParcel(int slotNumber, Parcel parcel) {
-        if (slotNumber >= 1 && slotNumber <= NUMBER_OF_SLOTS) {
-            slots.set(slotNumber - 1, parcel);
+    public void storeParcel(Parcel parcel) {
+        int slotNumber = findAvailableSlot();
+        if (slotNumber != -1) {
+            slots.set(slotNumber, parcel);
+            System.out.println("Parcel stored in slot number " + (slotNumber + 1));
         } else {
-            System.out.println("Slot number " + slotNumber + " does not exist. Please choose a slot number between 1 and " + NUMBER_OF_SLOTS + ".");
+            System.out.println("No available slots. Cannot store parcel.");
         }
+    }
+
+    private int findAvailableSlot() {
+        for (int i = 0; i < NUMBER_OF_SLOTS; i++) {
+            if (slots.get(i) == null) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public Parcel retrieveParcel(int slotNumber) {
@@ -29,7 +45,49 @@ public class ParcelManagementSystem {
         }
     }
 
+    public ArrayList<Parcel> checkAndReturnUnCollectedParcels() {
+        ArrayList<Parcel> uncollectedParcels = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_SLOTS; i++) {
+            Parcel parcel = slots.get(i);
+            if (parcel != null && parcel.getReceiver().equals(parcel.getSender())) {
+                uncollectedParcels.add(parcel);
+                slots.set(i, null);
+            }
+        }
+        return uncollectedParcels;
+    }
+
+    public void loadUserInformationFromFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userInfo = line.split(",");
+                if (userInfo.length == 2) {
+                    String sender = userInfo[0].trim();
+                    String receiver = userInfo[1].trim();
+                    Parcel parcel = new Parcel(sender, receiver);
+                    storeParcel(parcel);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error loading user information from file: " + e.getMessage());
+        }
+    }
+
+    public void saveUserInformationToFile(String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (Parcel parcel : slots) {
+                if (parcel != null) {
+                    writer.write(parcel.getSender() + "," + parcel.getReceiver());
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving user information to file: " + e.getMessage());
+        }
+    }
 }
+
 
 class Parcel {
     private String sender;
@@ -38,6 +96,14 @@ class Parcel {
     public Parcel(String sender, String receiver) {
         this.sender = sender;
         this.receiver = receiver;
+    }
+
+    public Object getSender() {
+        return null;
+    }
+
+    public Object getReceiver() {
+        return null;
     }
 
     // Add getters and setters as needed
