90047b97ebb0e13c2a2e5a6b06f18aae1c271bef
Amrabila <nuramiranabila03@gmail.com>
2023-12-24 11:04:55 +0800

    update main and parcelstorage

--- Main.java
+++ Main.java
@@ -1,51 +1,61 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         // Creating a Scanner object for user input
         Scanner scanner = new Scanner(System.in);
 
+        String[] recipientNames = new String[30]; // 1D array to store user names
+        int[] houseNumbers = new int[30]; // 1D array to store house numbers
+        Parcel[][] poBoxes = new Parcel[30][3]; // 2D array to store Parcel objects
+
         // Taking user input for creating a Parcel object
         System.out.println("Enter recipient name:");
-        String recipientName = scanner.nextLine();
+        String inputRecipientName = scanner.nextLine();
 
         System.out.println("Enter house number:");
-        int houseNumber = scanner.nextInt();
+        int inputHouseNumber = scanner.nextInt();
         scanner.nextLine(); // Consume the newline character
 
+        // System edit need to KIV
         System.out.println("Enter parcel content:");
         String content = scanner.nextLine();
 
         // Creating a Parcel object based on user input
-        Parcel parcel = new Parcel(recipientName, houseNumber, content);
+        Parcel parcel = new Parcel(inputRecipientName, inputHouseNumber, content);
+
+        // Storing the Parcel object in the 2D array
+        recipientNames[inputHouseNumber - 1] = inputRecipientName; // assuming house numbers start from 1
+        houseNumbers[inputHouseNumber - 1] = inputHouseNumber; // storing house number
+        poBoxes[inputHouseNumber - 1][0] = parcel; // storing in the first column, adjust as needed
 
         // Displaying initial information
         System.out.println("Initial Parcel Details:");
-        System.out.println(parcel);
-        System.out.println("Is the parcel expired? " + parcel.isExpired());
-        System.out.println("Parcel cost: $" + parcel.getParcelCost());
+        System.out.println(poBoxes[inputHouseNumber - 1][0]);
+        System.out.println("Is the parcel expired? " + poBoxes[inputHouseNumber - 1][0].isExpired());
+        System.out.println("Parcel cost: $" + poBoxes[inputHouseNumber - 1][0].getParcelCost());
         System.out.println();
 
         // Handling extend time for the parcel
         System.out.println("Handling extend time for 3 days...");
-        parcel.handleExtendTime(3);
+        poBoxes[inputHouseNumber - 1][0].handleExtendTime(3);
         System.out.println("Updated Parcel Details:");
-        System.out.println(parcel);
-        System.out.println("Is the parcel expired? " + parcel.isExpired());
-        System.out.println("Parcel cost: $" + parcel.getParcelCost());
+        System.out.println(poBoxes[inputHouseNumber - 1][0]);
+        System.out.println("Is the parcel expired? " + poBoxes[inputHouseNumber - 1][0].isExpired());
+        System.out.println("Parcel cost: $" + poBoxes[inputHouseNumber - 1][0].getParcelCost());
         System.out.println();
 
         // Handling return of the parcel
         System.out.println("Handling return of the parcel...");
-        parcel.handleReturn();
+        poBoxes[inputHouseNumber - 1][0].handleReturn();
         System.out.println("Updated Parcel Details:");
-        System.out.println(parcel);
-        System.out.println("Is the parcel expired? " + parcel.isExpired());
-        System.out.println("Parcel cost: $" + parcel.getParcelCost());
+        System.out.println(poBoxes[inputHouseNumber - 1][0]);
+        System.out.println("Is the parcel expired? " + poBoxes[inputHouseNumber - 1][0].isExpired());
+        System.out.println("Parcel cost: $" + poBoxes[inputHouseNumber - 1][0].getParcelCost());
 
-        // Closing the scanner
+        // Closing the scanner (optional)
         scanner.close();
     }
+}
 
-} // Closing curly brace for the class body
+
--- ParcelStorage.java
+++ ParcelStorage.java
@@ -1,13 +1,47 @@
 //ParcelStorage.java
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ParcelStorage {
    
-    private static final int MAX_SLOTS = 30;
+    //private static final int MAX_SLOTS = 30;
 
 
-    private static final int
+    //private static final int
 
-
+    //Method to load data from file
+    public static void loadDataFromFile(String[] recipientNames, String[][] poBox, String[] houseNumber) throws IOException {
+        
+        File file = new File("data.txt");
+     
+        int count = 0;
+        
+        try //Using exception to create a new file
+            (Scanner in = new Scanner (file)){
+        
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            String[] data = line.split(",");
+            recipientNames[count] = data[0]; //read recipient name
+            houseNumber[count] = data[1];    //read house number
+            poBox[count][0] = data[2];  //read the list of properties data,   //data[][0] = recipientNames
+            poBox[count][1] = data[3];                                        //data[][1] = houseNumber
+            poBox[count][2] = data[4];                                        //data[][2] = total units parcelbox that have been occupied
+            
+            count++; //increment count to read the next index 
+        }
+        
+        
+        in.close();
+        }
+        
+        catch(FileNotFoundException e){
+            System.out.println("Data file you search is not found. Continue to create a new file.");
+            System.out.println("File Succesfully created.");
+        }
+    }
 }
