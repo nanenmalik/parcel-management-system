32a3d45a0f8eb6270ee18023af6c8fbbc05266d6
Amrabila <nuramiranabila03@gmail.com>
2023-12-26 09:08:40 +0800

    edit main

--- Main.java
+++ Main.java
@@ -15,17 +15,18 @@ public class Main {
 
         // load data from file if it already exists
         ParcelStorage.loadDataFromFile( poBoxes[][]);         //call from PropertySystem file
-        System.out.println("Welcome to Property Management\n");
-
         do{
-            // Taking user input for creating a Parcel object
-            // System.out.println("Enter recipient name:");
-            // String inputRecipientName = scanner.nextLine();
+            System.out.println("Welcome to Property Management\n");
+
+            System.out.println("------ View PO Box ------");
 
             System.out.println("Enter house number:");
             houseNumbers = input.nextInt();
             
             if( houseNumbers>=1 || houseNumbers<=30 ){
+
+                System.out.println("Hello " + poBoxes[houseNumbers][0]);
+            
                 houseNumbers = houseNumbers - 1 ;
                 System.out.println("You have " + poBoxes[houseNumbers][4] + " parcel in your mail.");
 
@@ -37,27 +38,41 @@ public class Main {
                 else if( poBoxes[houseNumbers][5] == "NOTEXPIRED"){
                     System.out.println("Your parcel are" ); // + countdown day
 
-                    do{
+                    char answer;
+                    do {
                         int day = 0;
                         int timeExtended = 0;
-
-                        System.out.println("Do you wish to extend time of your parcel package?");
-                        System.out.println("Your time is " + poBoxes[houseNumbers][6].getParcelCost());
-
-                        day = input.nextInt();
-
-                        timeExtended = Parcel.handleExtendTime( days );
-
+            
+                        System.out.println("Do you wish to extend time for your parcel package?");
                         
-
-                    }while()
+                        // Input validation
+                        while (true) {
+                            try {
+                                day = input.nextInt();
+                                break; // Break out of the loop if input is an integer
+                            } catch (java.util.InputMismatchException e) {
+                                System.out.println("Invalid input. Please enter a valid integer.");
+                                input.next(); // Consume the invalid input
+                            }
+                        }
+            
+                        timeExtended = poBoxes[houseNumbers][6].handleExtendTime(day);
+                        System.out.println("The cost is RM" + (day * 3));
+                        System.out.println("Your time has been extended to " + timeExtended);
+            
+                        System.out.println("Do you want to re-do? (Y/N): ");
+                        answer = input.next().charAt(0);
+            
+                    } while (Character.toUpperCase(answer) == 'Y');
                 }
 
                 else if( poBoxes[houseNumbers][5] == "NOPARCEL"){
                     System.out.println("There are no parcel in your mail." ); // + countdown day
                 }
 
-                
+                System.out.println("Do you want to view your PO Box information?");
+                //put view data
+
             }
 
         }while
