3061c5098135f7957db25730bfee9e5bd3d26675
nanenmalik <nasreen.malik@live.iium.edu.my>
2023-12-26 11:29:11 +0800

    update and modify parcel class

--- Parcel.java
+++ Parcel.java
@@ -4,16 +4,16 @@ import java.time.temporal.ChronoUnit;
 public class Parcel {
     private String recipientName;
     private int houseNumber;
-    private String content;
     private LocalDate storedDate;
     private double parcelCost;
+    private int countdownTime;
 
     public Parcel(String recipientName, int houseNumber, String content) {
         this.recipientName = recipientName;
         this.houseNumber = houseNumber;
-        this.content = content;
         this.storedDate = LocalDate.now();
         this.parcelCost = 0;
+        this.countdownTime = 0;
     }
 
     public int getHouseNumber() {
@@ -24,29 +24,50 @@ public class Parcel {
         return recipientName;
     }
 
-       public boolean isExpired() {
+    public LocalDate getStoredDate() {
+        return storedDate;
+    }
+
+    public boolean isExpired() {
         LocalDate currentDate = LocalDate.now();
         long daysDifference = ChronoUnit.DAYS.between(storedDate, currentDate);
-        return daysDifference > 2;
+
+        if (daysDifference > 2) {
+            countdownTime = 0;
+            return true;
+        } else if (daysDifference == 2) {
+            countdownTime = 1;
+            return false;
+        } else {
+            countdownTime = 2;
+            return false;
+        }
     }
-    
-    public void handleExtendTime(int days) {
+
+    public int getCountdownTime() {
+        return countdownTime;
+    }
+
+    public double handleExtendTime(int days) {
         storedDate = storedDate.plusDays(days);
         parcelCost += days * 3;
+        return parcelCost;
     }
-    
+
     public void handleReturn() {
         storedDate = LocalDate.now();
     }
-       
+
     public double getParcelCost() {
         return parcelCost;
     }
-       
 
     @Override
     public String toString() {
-        return "Parcel for " + recipientName + " at " + houseNumber + " containing " + content;
+        if (isExpired()) {
+            return "Expired Parcel for " + recipientName + " at " + houseNumber + " with countdown time: " + getCountdownTime();
+        } else {
+            return "Not Expired Parcel for " + recipientName + " at " + houseNumber + " with countdown time: " + getCountdownTime();
+        }
     }
-   
 }
