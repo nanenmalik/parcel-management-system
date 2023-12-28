eccefba8991a4e3136cdf80362f69e30bcf1e67f
nanenmalik <nasreen.malik@live.iium.edu.my>
2023-12-28 11:32:22 +0800

    modify Parcel class

--- Parcel.java
+++ Parcel.java
@@ -1,107 +1,72 @@
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 public class Parcel {
-    private String[] recipient ;
-    private int houseNumber;
+
+    // Represents a parcel with a username, collection code, stored date, expired date, and sender.
+    private String username;
+    private String code;
     private LocalDate storedDate;
-    public int parcelCost;
-    private int countdownTime;
-    private String[][] poBox;
+    private LocalDate expiredDate;
+    private String sender;
 
-    public Parcel(String[] recipient, int houseNumber, String[][] poBox) {
-        this.recipient = recipient;
-        this.houseNumber = houseNumber;
+    
+    // Creates a new parcel with the given username, code, and sender.
+    public Parcel(String username, String code, String sender) {
+        this.username = username;
+        this.code = code;
         this.storedDate = LocalDate.now();
-        this.parcelCost = 0;
-        this.countdownTime = 0;
+        this.expiredDate = storedDate.plusDays(2);
+        this.sender = sender;
+    }
+    
+    public Parcel(String username, String code, LocalDate storedDate, LocalDate expiredDate, String sender) {
+        this.username = username;
+
+        this.code = code;
+        this.storedDate = storedDate;
+        this.expiredDate = expiredDate;
+        this.sender = sender;
     }
 
-    public Parcel(Object recipient2, Object houseNumber2, Object poBox2) {
+    public String getusername() { // Get the username.
+        return username;
     }
 
-    public int getHouseNumber() { //getter
-        return houseNumber;
+    public void setusername(String username) 
+    
+    { // Set the house username.
+        this.username = username;
     }
 
-    public String[] getRecipient() { 
-        return recipient;
+    public String getCode() { // Get the code.
+        return code;
     }
 
-    public LocalDate getStoredDate() { 
+    public void setCode(String code) { // Set the code.
+        this.code = code;
+    }
+
+    public LocalDate getStoredDate() { // Get the stored date.
         return storedDate;
     }
 
-    //time,day left ,parcel status
-    public int handleExtendTime(int days) { 
-        storedDate = storedDate.plusDays(days);
-        parcelCost += days * 3;
-
-        if(days == 0){
-            String time = "Extend";
-            poBox[houseNumber][6] = time;
-        }
-
-        return parcelCost;
+    public void setStoredDate(LocalDate storedDate) { // Set the stored date.
+        this.storedDate = storedDate;
     }
 
-
-
-    public int getCountdownTime() {
-        if(countdownTime == 0){
-            String dayLeft = "EXPIRED";
-            poBox[houseNumber][4] = dayLeft;
-        }
-        else if(countdownTime == 1){
-            String dayLeft = "1 DAY LEFT";
-            poBox[houseNumber][4] = dayLeft;
-        }
-        else if(countdownTime == 2){
-            String dayLeft = "2 DAYS LEFT";
-            poBox[houseNumber][4] = dayLeft;
-
-        } 
-        return countdownTime;
+    public LocalDate getExpiredDate() { // Get the expired date.
+        return expiredDate;
     }
 
-    public boolean isExpired() { 
-        LocalDate currentDate = LocalDate.now();
-        long daysDifference = ChronoUnit.DAYS.between(storedDate, currentDate);
-
-
-        if (daysDifference > 2) {
-            countdownTime = 0;
-            String parcelStatus = "EXPIRED";
-            poBox[houseNumber][6] = parcelStatus;
-            return true;
-        } else if (daysDifference == 2) {
-            countdownTime = 1;
-            String parcelStatus = "NOT EXPIRED";
-             poBox[houseNumber][6] = parcelStatus;
-            return false;
-        } else {
-            countdownTime = 2;
-            String parcelStatus = "NOT EXPIRED";
-             poBox[houseNumber][6] = parcelStatus;
-            return false;
-        }
+    public void setExpiredDate(LocalDate expiredDate) { // Set the expired date.
+        this.expiredDate = expiredDate;
     }
 
-      
-    public void handleReturn() { 
-        storedDate = LocalDate.now();
+    public String getSender() { // Get the sender.
+        return sender;
     }
 
-    public double getParcelCost() { 
-        return parcelCost;
+    public void setSender(String sender) { // Set the sender.
+        this.sender = sender;
     }
-
-    /*@Override
-    public String toString() {
-        String parcelInfo = "Parcel for " + recipientName + " at " + houseNumber + " with countdown time: " + getCountdownTime();
-        if (isExpired()) {
-        parcelInfo = "Expired " + parcelInfo;
-        }
-         return parcelInfo;
-    }*/
 }
